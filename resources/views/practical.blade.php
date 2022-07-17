38e892910a5517e25a711cc731867c00e02e9397
reigningkingforever <reigningkingforever@gmail.com>
2022-07-17 05:47:01 +0100

    homepage done

---
+++ resources/views/practical.blade.php
@@ -0,0 +1,42 @@
+<section   style="background-color: #f6f7f8; ">
+    <div class="container-fluid">
+        <div class="vc_row vc_row_fluid    has-fullwidth-column-no-padding ">
+            <div class="wpb_column agni_column_container agni_column vc_column_container vc_col-sm-6 vc_custom_1479233898132">
+                <div class="agni_column-inner vc_column-inner text-center">
+                    <div class="wpb_wrapper">
+                        <div class="vc_empty_space"   style="height: 500px">
+                            <span class="vc_empty_space_inner"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="wpb_column agni_column_container agni_column vc_column_container vc_col-sm-6 vc_custom_1478872925659">
+                <div class="agni_column-inner vc_column-inner text-left">
+                    <div class="wpb_wrapper">
+                        <div class="vc_empty_space"   style="height: 25px"><span class="vc_empty_space_inner"></span></div>
+                        <div class="wpb_text_column wpb_content_element " >
+                            <div class="wpb_wrapper">
+                                <h2 style="text-align: left;">Practical, People-Focused Solutions for Your Business</h2>
+
+                            </div>
+                        </div>
+                        <div class=" separator separator_left" style="width:15%; ">
+                            <span class="sep_holder sep_holder_l"><span style="border-top-style:solid; border-color:#1595d3" class="sep_line"></span></span>
+                            
+                            <span class="sep_holder sep_holder_r"><span style="border-top-style:solid; border-color:#1595d3" class="sep_line"></span></span>
+                        </div>
+                        <div class="wpb_text_column wpb_content_element " >
+                            <div class="wpb_wrapper">
+                                <p style="text-align: left;">In today’s business environment, technology is key to innovation, collaboration and growth. You need a managed service provider that understands your unique needs, plus has the practical experience necessary to implement solutions that set you up for success—today and tomorrow.</p>
+
+                            </div>
+                        </div>
+                        <div class="vc_empty_space"   style="height: 25px">
+                            <span class="vc_empty_space_inner"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ resources/views/specialize.blade.php
@@ -0,0 +1,217 @@
+<section   style="background-image: url({{asset('public/wp-content/uploads/2016/10/gem-icon-bg-home.jpg')}}); background-repeat:repeat; background-size:cover; background-position:center center; background-attachment:scroll; ">
+    <div class="container">
+        <div class="vc_row vc_row_fluid     ">
+            <div class="wpb_column agni_column_container agni_column vc_column_container vc_col-sm-12">
+                <div class="agni_column-inner vc_column-inner text-left">
+                    <div class="wpb_wrapper"><div class="vc_empty_space"   style="height: 75px">
+                        <span class="vc_empty_space_inner"></span>
+                    </div>
+                    <div class="wpb_text_column wpb_content_element " >
+                        <div class="wpb_wrapper">
+                            <h2 style="text-align: center;">We specialize in IT support for:</h2>
+
+                        </div>
+                    </div>
+                    <div class="vc_inner">
+                        <div class="vc_row vc_row-fluid  ">
+                            <div class="wpb_column agni_column_container vc_column_container vc_col-sm-3">
+                                <div class="agni_column-inner vc_column-inner text-center">
+                                    <div class="wpb_wrapper">
+                                        <div class="wpb_text_column wpb_content_element " >
+                                            <div class="wpb_wrapper">
+                                                <p><a href="industries/advertising-and-media/index.html"></p>
+
+                                            </div>
+                                        </div>
+                                        <span class=" agni-icon " style="border-radius:50%; color:#002f65; border-color:#002f65; "><i class="ion-videocamera icon-has-border hover-icon-has-border" style="font-size:36px; border-radius:0%; color:#002f65; width:85px; height:85px; border-color:#002f65; "></i></span>
+                                        <div class="wpb_text_column wpb_content_element " >
+                                            <div class="wpb_wrapper">
+                                                <p></a></p>
+                                                <h5><a href="industries/advertising-and-media/index.html">Advertising and Media</a></h5>
+
+                                            </div>
+                                        </div>
+
+                                        <div class="wpb_text_column wpb_content_element " >
+                                            <div class="wpb_wrapper">
+                                                
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="wpb_column agni_column_container vc_column_container vc_col-sm-3">
+                                <div class="agni_column-inner vc_column-inner text-center">
+                                    <div class="wpb_wrapper">
+                                        <div class="wpb_text_column wpb_content_element " >
+                                            <div class="wpb_wrapper">
+                                                <p><a href="industries/financial/index.html"></p>
+
+                                            </div>
+                                        </div>
+                                        <span class=" agni-icon " style="border-radius:50%; color:#002f65; border-color:#002f65; "><i class="fa fa-money icon-has-border hover-icon-has-border" style="font-size:36px; border-radius:0%; color:#002f65; width:85px; height:85px; border-color:#002f65; "></i></span>
+                                        <div class="wpb_text_column wpb_content_element " >
+                                            <div class="wpb_wrapper">
+                                                <p></a></p>
+                                                <h5><a href="industries/financial/index.html">Financial and Accounting</a></h5>
+
+                                            </div>
+                                        </div>
+
+                                        <div class="wpb_text_column wpb_content_element " >
+                                            <div class="wpb_wrapper">
+                                                
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="wpb_column agni_column_container vc_column_container vc_col-sm-3"><div class="agni_column-inner vc_column-inner text-center"><div class="wpb_wrapper">
+                            <div class="wpb_text_column wpb_content_element " >
+                                <div class="wpb_wrapper">
+                                    <p><a href="industries/legal-services/index.html"></p>
+
+                                </div>
+                            </div>
+                            <span class=" agni-icon " style="border-radius:50%; color:#002f65; border-color:#002f65; "><i class="fa fa-legal icon-has-border hover-icon-has-border" style="font-size:36px; border-radius:0%; color:#002f65; width:85px; height:85px; border-color:#002f65; "></i></span>
+                            <div class="wpb_text_column wpb_content_element " >
+                                <div class="wpb_wrapper">
+                                    <p></a></p>
+                                    <h5><a href="industries/legal-services/index.html">Legal Services</a></h5>
+
+                                </div>
+                            </div>
+
+                            <div class="wpb_text_column wpb_content_element " >
+                                <div class="wpb_wrapper">
+                                    
+                                </div>
+                            </div>
+                            </div>
+                            </div></div>
+                            <div class="wpb_column agni_column_container vc_column_container vc_col-sm-3">
+                                <div class="agni_column-inner vc_column-inner text-center">
+                                    <div class="wpb_wrapper">
+                                        <div class="wpb_text_column wpb_content_element " >
+                                            <div class="wpb_wrapper">
+                                                <p><a href="industries/healthcare-it-services/index.html"></p>
+
+                                            </div>
+                                        </div>
+                                        <span class=" agni-icon " style="border-radius:50%; color:#002f65; border-color:#002f65; "><i class="fa fa-medkit icon-has-border hover-icon-has-border" style="font-size:36px; border-radius:0%; color:#002f65; width:85px; height:85px; border-color:#002f65; "></i></span>
+                                        <div class="wpb_text_column wpb_content_element " >
+                                            <div class="wpb_wrapper">
+                                                <p></a></p>
+                                            <h5><a href="industries/healthcare-it-services/index.html">Healthcare and Medical</a></h5>
+
+                                            </div>
+                                        </div>
+
+                                        <div class="wpb_text_column wpb_content_element " >
+                                            <div class="wpb_wrapper">
+                                                
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="vc_empty_space"   style="height: 25px"><span class="vc_empty_space_inner"></span></div>
+                    <div class="vc_inner">
+                        <div class="vc_row vc_row-fluid  ">
+                            <div class="wpb_column agni_column_container vc_column_container vc_col-sm-3">
+                                <div class="agni_column-inner vc_column-inner text-center">
+                                    <div class="wpb_wrapper">
+                                        <div class="wpb_text_column wpb_content_element " >
+                                            <div class="wpb_wrapper">
+                                                <p><a href="industries/construction/index.html"></p>
+
+                                            </div>
+                                        </div>
+                                        <span class=" agni-icon " style="border-radius:50%; color:#002f65; border-color:#002f65; "><i class="pe-7f-vector icon-has-border hover-icon-has-border" style="font-size:36px; border-radius:0%; color:#002f65; width:85px; height:85px; border-color:#002f65; "></i></span>
+                                        <div class="wpb_text_column wpb_content_element " >
+                                            <div class="wpb_wrapper">
+                                                <p></a></p>
+                                            <h5><a href="industries/construction/index.html">Construction</a></h5>
+
+                                            </div>
+                                        </div>
+
+                                        <div class="wpb_text_column wpb_content_element " >
+                                            <div class="wpb_wrapper">
+                                                
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="wpb_column agni_column_container vc_column_container vc_col-sm-3">
+                                <div class="agni_column-inner vc_column-inner text-center">
+                                    <div class="wpb_wrapper">
+                                        <div class="wpb_text_column wpb_content_element " >
+                                            <div class="wpb_wrapper">
+                                                <p><a href="industries/government/index.html"></p>
+
+                                            </div>
+                                        </div>
+                                        <span class=" agni-icon " style="border-radius:50%; color:#002f65; border-color:#002f65; ">
+                                        <i class="ion-android-checkbox-outline icon-has-border hover-icon-has-border" style="font-size:36px; border-radius:0%; color:#002f65; width:85px; height:85px; border-color:#002f65; "></i></span>
+                                        <div class="wpb_text_column wpb_content_element " >
+                                            <div class="wpb_wrapper">
+                                                <p></a></p>
+                                                <h5><a href="industries/government/index.html">Government</a></h5>
+
+                                            </div>
+                                        </div>
+
+                                        <div class="wpb_text_column wpb_content_element " >
+                                            <div class="wpb_wrapper">
+                                                
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="wpb_column agni_column_container vc_column_container vc_col-sm-3">
+                                <div class="agni_column-inner vc_column-inner text-center">
+                                    <div class="wpb_wrapper">
+                                        <div class="wpb_text_column wpb_content_element " >
+                                            <div class="wpb_wrapper">
+                                                <p><a href="industries/non-profit/index.html"></p>
+
+                                            </div>
+                                        </div>
+                                        <span class=" agni-icon " style="border-radius:50%; color:#002f65; border-color:#002f65; "><i class="ion-ribbon-b icon-has-border hover-icon-has-border" style="font-size:36px; border-radius:0%; color:#002f65; width:85px; height:85px; border-color:#002f65; "></i></span>
+                                            <div class="wpb_text_column wpb_content_element " >
+                                                <div class="wpb_wrapper">
+                                                    <p></a></p>
+                                                    <h5><a href="industries/non-profit/index.html">Non-Profit</a></h5>
+
+                                                </div>
+                                            </div>
+
+                                        <div class="wpb_text_column wpb_content_element " >
+                                            <div class="wpb_wrapper">
+                                                
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="wpb_column agni_column_container vc_column_container vc_col-sm-3">
+                                <div class="agni_column-inner vc_column-inner text-left"><div class="wpb_wrapper">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="vc_empty_space"   style="height: 50px">
+                        <span class="vc_empty_space_inner"></span>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
